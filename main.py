eb9dc05b186f835ef116b262e1c23a45b7c35fde
Gunner-Bones <35184209+Gunner-Bones@users.noreply.github.com>
2020-08-24 13:16:30 -0500

    Pointercrate DLC expansion
    Added getting far % support for list demons, and the ability to change the getting far % in discord.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import gd
 import time
 import sys
+import os
 import discord
 import asyncio
 import random
+import json
 import ctypes
 import ctypes.util
+import urllib.request as request
 from discord.ext import commands
 
 """lib_opus = ["libopus-0.x64.dll","libopus-0.x86.dll"]
@@ -63,6 +66,15 @@ def strtolist(s):
         	st[st.index(t)] = int(t)
     return st
 
+def corePYIPath(relative):
+    return os.path.join(
+        os.environ.get(
+            "_MEIPASS2",
+            os.path.abspath(".")
+        ),
+        relative
+    )
+
 def datasettings(file,method,line="",newvalue="",newkey=""):
 	"""
 	:param file: (str).txt
@@ -71,6 +83,7 @@ def datasettings(file,method,line="",newvalue="",newkey=""):
 	:param newvalue: (str)
 	:param newkey: (str)
 	"""
+	file = corePYIPath(file)
 	s = None
 	try: s = open(file,"r")
 	except: return None
@@ -108,12 +121,33 @@ BOT_OWNER = None
 TIMEOUT = int(datasettings(file="settings.txt",method="get",line="TIMEOUT"))
 GETTING_FAR = int(datasettings(file="settings.txt",method="get",line="GETTING_FAR"))
 FAR_CHANNEL = strtolist(datasettings(file="settings.txt",method="get",line="FAR_CHANNEL"))
+LIST_RQ = int(datasettings(file="settings.txt",method="get",line="LIST_RQ"))
 
 
 DEAFEN = False
 VOICE_SESSION = None
 SCANNING = False
+LIST_RF = []
 
+
+def requestPCDemonRequirement(level_name):
+	global LIST_RF
+	if not LIST_RF:
+		url1 = "https://pointercrate.com/api/v1/demons/?limit=100"
+		url2 = "https://pointercrate.com/api/v1/demons/?after=100"
+		req1 = request.urlopen(url1)
+		req2 = request.urlopen(url2)
+		rd1 = req1.read()
+		rd2 = req2.read()
+		js1 = json.loads(rd1.decode("utf-8"))
+		js2 = json.loads(rd2.decode("utf-8"))
+		demons = js1 + js2
+		LIST_RF = sorted(demons, key=lambda j: j['position'])
+		print("[Getting Far] Pointercrate Demon List refreshed.")
+	for demon in LIST_RF:
+		if demon['name'] == level_name:
+			return int(demon['requirement'])
+	return 30
 
 async def response_message(ctx, response, message_reaction, preset=""):
 	if preset != "":
@@ -127,8 +161,12 @@ async def response_message(ctx, response, message_reaction, preset=""):
 
 async def sendout_message(message_type, level_name, level_creator, level_perc=""):
 	global FAR_CHANNEL
+	global LIST_RQ
+	pflm = ""
+	if LIST_RQ:
+		pflm = ", List Requirement!"
 	pi = {"far": "**" + BOT_OWNER.name + "** is *getting far* on **__" + level_name + "__ by "
-	 + level_creator + "**...  (" + str(GETTING_FAR) + "%+)",
+	 + level_creator + "**...  (" + str(GETTING_FAR) + "%+" + pflm + ")",
 	"dead": random.choice(MES_DEATH) + "**" + BOT_OWNER.name + "** just died at *" + level_perc
 	 + "%* on **__" + level_name + "__ by " + level_creator + "**.",
 	 "win": random.choice(MES_WIN) + "**" + BOT_OWNER.name + "** just completed **__"
@@ -142,7 +180,6 @@ async def sendout_message(message_type, level_name, level_creator, level_perc=""
 			except discord.errors.Forbidden:
 				pass
 	return True
-
 
 def bot_permissions(ctx):
 	if not ctx.message.guild: return True
@@ -212,6 +249,48 @@ async def far_channel(ctx):
 	else:
 		await response_message(ctx,"","failed","bot_lacks_perms")
 
+
+@client.command(pass_context=True)
+async def toggle_list(ctx):
+	global LIST_RQ
+	if bot_permissions(ctx):
+		if bot_owner(ctx):
+			LIST_RQ = abs(LIST_RQ - 1)
+			if LIST_RQ:
+				await response_message(ctx, "Scanning for List Demon Requirement % turned `ON`.", "success")
+				print("[Getting Far] LIST_RQ turned ON")
+			else:
+				await response_message(ctx, "Scanning for List Demon Requirement % turned `OFF`.", "success")
+				print("[Getting Far] LIST_RQ turned OFF")
+			datasettings(file="settings.txt", method="change", line="LIST_RQ" , newvalue=str(LIST_RQ))
+		else:
+			await response_message(ctx,"","failed","author_not_owner")
+	else:
+		await response_message(ctx,"","failed","bot_lacks_perms")
+
+
+@client.command(pass_context=True)
+async def change_far(ctx, num):
+	global GETTING_FAR
+	if bot_permissions(ctx):
+		if bot_owner(ctx):
+			if is_num(num):
+				perc = int(num)
+				if 1 < perc < 100:
+					GETTING_FAR = perc
+					datasettings(file="settings.txt", method="change", line="GETTING_FAR", newvalue=str(perc))
+					await response_message(ctx, "GETTING FAR % changed to *" + str(perc) + "*.", "success")
+					print("[Getting Far] GETTING_FAR=" + str(perc) + "%")
+				else:
+					await response_message(ctx, "Number must be between 1-100!", "failed")
+			else:
+				await response_message(ctx, "*" + num + "* is not a number!", "failed")
+		else:
+			await response_message(ctx,"","failed","author_not_owner")
+	else:
+		await response_message(ctx,"","failed","bot_lacks_perms")
+
+
 @client.event
 async def on_voice_state_update(member,before,after):
 	global SCANNING
@@ -227,6 +306,8 @@ async def main():
 	global VOICE_SESSION
 	global DEAFEN
 	global BOT_OWNER
+	global LIST_RQ
+	global GETTING_FAR
 	level_name = "None"
 	level_creator = "None"
 	level_best = 0
@@ -248,11 +329,20 @@ async def main():
 		pass
 	alive = True
 	victory = False
+	changed_rq = False
 	while True:
 		if SCANNING and memory.is_in_level():
 			level_name = memory.get_level_name()
 			level_creator = memory.get_level_creator()
 			level_best = memory.get_normal_percent()
+			if LIST_RQ and not changed_rq:
+				if memory.get_level_stars() == 10 and memory.get_level_demon_difficulty_value() >= 5:
+					new_requirement = requestPCDemonRequirement(level_name)
+					GETTING_FAR = new_requirement
+					datasettings(file="settings.txt", method="change", line="GETTING_FAR", newvalue=str(new_requirement))
+					changed_rq = True
+					print("[Getting Far] LIST_RQ on, detected List Demon '" + level_name + 
+						"' with Requirement " + str(new_requirement) + "%")
 			if not DEAFEN and memory.get_percent() >= GETTING_FAR and not memory.is_dead():
 				DEAFEN = True
 				await VOICE_SESSION.guild.get_member(BOT_OWNER.id).edit(deafen=True)
@@ -269,7 +359,7 @@ async def main():
 				if memory.get_percent() >= GETTING_FAR:
 					await sendout_message(message_type="dead", level_name=level_name, level_creator=level_creator, 
 						level_perc=str(round(memory.get_percent())))
-				print("[Getting Far] User DIED!")
+					print("[Getting Far] User DIED FAR! " + str(round(memory.get_percent())) + "% on '" + level_name + "', Attempt " + str(memory.get_attempt()))
 			if not memory.is_dead() and not alive:
 				alive = True
 			if memory.get_percent() == 100 and not victory:
@@ -279,6 +369,7 @@ async def main():
 				print("[Getting Far] User WON!!!")
 		elif not memory.is_in_level():
 			victory = False
+			changed_rq = False
 		await asyncio.sleep(0.5)
 
 
